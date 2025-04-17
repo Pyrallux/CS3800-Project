2de7d9ab4f1b38cf946f19e0f83738d5e8a10b91
Pyrallux <nnnevwa630@gmail.com>
2025-04-16 21:41:39 -0500

    Implement CUD Files in C++

---
+++ C++/SingleThreaded/cudFiles.cpp
@@ -0,0 +1,55 @@
+#include <chrono>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std::chrono;
+
+std::vector<int> generateRandList(int size) {
+  std::vector L(size, 0);
+  for (int i = 0; i < L.size(); i++) {
+    L[i] = std::rand();
+  }
+  return L;
+}
+
+void writeFile(const std::string &filename, int contents) {
+  std::ofstream outputFile(filename);
+  outputFile << contents << std::endl;
+  outputFile.close();
+}
+
+int readFile(const std::string &filename) {
+  std::ifstream inputFile(filename);
+  std::string text;
+  std::getline(inputFile, text);
+  inputFile.close(); // Close the file when done
+  return std::stoi(text);
+}
+
+void createReadDelete(const std::string &filename, int contents) {
+  writeFile(filename, contents);
+  readFile(filename);
+  std::remove(filename.c_str()); // Deletes File
+}
+
+int main() {
+  // Setup and Generate Random number lists
+  std::srand(std::time(0));
+  std::vector<int> RandList = generateRandList(16);
+
+  // Perform selection sorts and measure time
+  auto start = high_resolution_clock::now();
+  for (int i = 0; i < RandList.size(); i++) {
+    std::string name = "randomFile" + std::to_string(i) + ".txt";
+    createReadDelete(name, RandList[i]);
+  }
+  auto stop = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(stop - start);
+
+  // Print Results
+  std::cout << static_cast<float>(duration.count()) / 1000000 << std::endl;
+
+  return 0;
+}
--- run.sh
+++ run.sh
@@ -17,6 +17,8 @@ g++ ./C++/SingleThreaded/selSort.cpp -o singleSelSort.o
 g++ ./C++/MultiThreaded/selSort.cpp -o multiSelSort.o
 g++ ./C++/SingleThreaded/webDownload.cpp -o singleWebDownload.o -lcurl
 g++ ./C++/MultiThreaded/webDownload.cpp -o multiWebDownload.o -lcurl
+g++ ./C++/SingleThreaded/cudFiles.cpp -o singleCUD.o 
+g++ ./C++/MultiThreaded/cudFiles.cpp -o multiCUD.o 
 
 # Selection Sort
 echo "--------------------"
@@ -56,7 +58,24 @@ echo -e "  Time ${result} by multi-threading: ${color}${webDownloadDiff}${NC}s"
 rm "./singleWebDownload.o"
 rm "./multiWebDownload.o"
 
-# Writing to File
+# Create, Read, and Delete Files
+echo "--------------------"
+echo "Create, Read, and Delete Files:"
+singleCUD=$(./singleCUD.o)
+multiCUD=$(./multiCUD.o)
+CUDDiff=$(echo "$singleCUD - $multiCUD" | bc)
+if [ "$(echo "$CUDDiff > 0" | bc)" -eq 1 ]; then
+    color=$GREEN
+    result="saved"
+else 
+    color=$RED
+    result="lost"
+fi
+echo "  Execution Time with 1 Thread: ${singleCUD}s"
+echo "  Execution Time with 16 Threads: ${multiCUD}s"
+echo -e "  Time ${result} by multi-threading: ${color}${CUDDiff}${NC}s"
+rm "./singleCUD.o"
+rm "./multiCUD.o"
 
 
 
